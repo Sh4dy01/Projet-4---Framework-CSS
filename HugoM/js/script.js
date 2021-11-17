ace0d97a9878f3cb2558b30b73cb507f7fcfaeb6
Hugo <maestraccihugofacebook@yahoo.fr>
2021-11-17 17:17:48 +0100

    Modal & Navbar

--- HugoM/js/script.js
+++ HugoM/js/script.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
   $('.dropdown-trigger').dropdown();
   $('.materialboxed').materialbox();
+  $('input#input_text, textarea#textarea2').characterCounter();
 });
 
-var dropdown = $("#projects-btn");
-dropdown.alignment("right");
+$('#textarea1').val('');
+ M.textareaAutoResize($('#textarea1'));
