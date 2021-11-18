a6cf59e74bd21c30f7e4c0bd70f226ca84828965
judegoiave <bmillan@gaming.tech>
2021-11-18 23:06:31 +0100

    pas mal de choses carrousel pour le site + photos

--- BaptisteM/js/script.js
+++ BaptisteM/js/script.js
@@ -3,7 +3,21 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
   $('.dropdown-trigger').dropdown();
   $('.materialboxed').materialbox();
+  $('input#input_text, textarea#textarea2').characterCounter();
+  $('.collapsible').collapsible();
+  $('.carousel').carousel();
+  $('.scrollspy').scrollSpy();
 });
+
 
 var dropdown = $("#projects-btn");
 dropdown.alignment("right");
+
+
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: true
+});
+
+$('#textarea1').val('');
+M.textareaAutoResize($('#textarea1'));
