a2b7b40ffcd9497114d69c51a21d9e8456fa6c84
Hugo <maestraccihugofacebook@yahoo.fr>
2021-11-18 12:55:57 +0100

    Project section #2

--- HugoM/js/script.js
+++ HugoM/js/script.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   $('.dropdown-trigger').dropdown();
   $('.materialboxed').materialbox();
   $('input#input_text, textarea#textarea2').characterCounter();
+  $('.collapsible').collapsible();
 });
 
 $('#textarea1').val('');
