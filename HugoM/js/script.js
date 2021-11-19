17bea0e75b1ac22fb5caa9272ac0080718738369
Hugo <maestraccihugofacebook@yahoo.fr>
2021-11-19 12:11:04 +0100

    Avancement

--- HugoM/js/script.js
+++ HugoM/js/script.js
@@ -6,13 +6,36 @@ $(document).ready(function(){
   $('input#input_text, textarea#textarea2').characterCounter();
   $('.collapsible').collapsible();
   $('.carousel').carousel();
-  $('.scrollspy').scrollSpy();
-});
-
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
 });
 
 $('#textarea1').val('');
- M.textareaAutoResize($('#textarea1'));
+  M.textareaAutoResize($('#textarea1'));
+
+$("#secret_btn" ).click(function() {
+  var text = $("#secret_text").val();
+  var elem = $("#secret_img");
+  var site = $("#main");
+
+  site.hide();
+  elem.show();
+  elem.animate({top: '-=100px'}, "slow");
+  elem.animate({left: '+=100px'}, "fast");
+  elem.animate({left: '-=100px'}, "slow");
+  elem.animate({left: '+=10000px'}, "fast");
+
+  if (text == 'adventure') {
+
+  }
+  else {
+  }
+});
+
+$("#return_btn" ).click(function() {
+  var site = $("#main");
+  var elem = $("#secret_img");
+
+  site.show();
+  elem.hide();
+  elem.css("bottom", "0");
+  elem.css("left", "0")
+});
